5165debabb9408eba58c136efbebc4da76b87c19
Mark Addis <nitro_charlie@live.com>
2022-04-21 12:24:14 +0100

    Add knexfile and create table

--- migrations/20220421110859_initial_tables.js
+++ migrations/20220421110859_initial_tables.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = async (knex) => {
   // check extension is not installed
   const [extInstalled] = await knex('pg_extension').select('*').where({ extname: 'uuid-ossp' })
@@ -11,10 +7,18 @@ exports.up = async (knex) => {
 
   const uuidGenerateV4 = () => knex.raw('uuid_generate_v4()')
   const now = () => knex.fn.now()
+
+  await knex.schema.createTable('profiler', (def) => {
+    def.uuid('id').defaultTo(uuidGenerateV4())
+    def.string('name', 50).unique().notNullable()
+    def.datetime('created_at').notNullable().default(now())
+    def.datetime('updated_at').notNullable().default(now())
+
+    def.primary(['id'])
+  })
 }
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function (knex) {}
+exports.down = async (knex) => {
+  await knex.schema.dropTable('profiler')
+  await knex.raw('DROP EXTENSION "uuid-ossp"')
+}
